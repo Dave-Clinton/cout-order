8b7696ea43f9c7295e07264642cf49e8d5b71d99
Masiza-Fidel <fidelmasitsa03@gmail.com>
2024-08-07 06:29:09 +0200

    commit

---
+++ myapp/admin.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from .models import Profile,Affidavit
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name')
+    search_fields = ('user__username', 'first_name', 'last_name')
+
+admin.site.register(Profile, ProfileAdmin)
+
+
+class AffidavitAdmin(admin.ModelAdmin):
+    list_display = ('profile','status' )
+    search_fields = ('profile__user__username','status' )
+
+admin.site.register(Affidavit, AffidavitAdmin)
